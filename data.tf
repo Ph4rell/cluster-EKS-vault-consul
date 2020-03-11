07f71ba0a99d3b5c1449849d1b1aaa3f4786d4cf
Pierre <pierre.poree@d2si.io>
2020-03-11 17:58:58 +0100

    add data to complete kubeconfig tpl

--- data.tf
+++ data.tf
@@ -1,3 +1,36 @@
 data "aws_availability_zones" "zone" {
   state = "available"
 }
+
+data "template_file" "kubeconfig" {
+  template = file("${path.module}/template/kubeconfig.tpl")
+
+  vars = {
+    endpoint                  = aws_eks_cluster.eks_cluster.endpoint
+    cluster_auth_base64       = aws_eks_cluster.eks_cluster.certificate_authority.0.data
+    kubeconfig_name           = local.prefix 
+    aws_authenticator_command = var.kubeconfig_aws_authenticator_command
+    aws_authenticator_command_args = "        - ${join(
+      "\n        - ",
+      formatlist("\"%s\"", ["token", "-i", aws_eks_cluster.eks_cluster.name]),
+    )}"
+    aws_authenticator_env_variables = "      env:\n${join(
+      "\n",
+      data.template_file.aws_authenticator_env_variables.*.rendered,
+    )}"
+}
+}
+
+data "template_file" "aws_authenticator_env_variables" {
+  count = length(var.kubeconfig_aws_authenticator_env_variables)
+
+  template = <<EOF
+        - name: $${key}
+          value: $${value}
+EOF
+
+  vars = {
+    value = values(var.kubeconfig_aws_authenticator_env_variables)[count.index]
+    key   = keys(var.kubeconfig_aws_authenticator_env_variables)[count.index]
+  }
+}

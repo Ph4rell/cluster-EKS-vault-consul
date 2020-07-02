f53bc44998e29806b73f5fac613017ef02868d00
Pierre POREE <pierre.poree@revolve.team>
2020-07-03 00:08:06 +0200

    move terraform files

--- terraform/kubectl.tf
+++ terraform/kubectl.tf
@@ -1,6 +1,6 @@
 resource "local_file" "kubeconfig" {
   content = data.template_file.kubeconfig.rendered
-  filename = "/Users/pierre.poree/.kube/config"
+  filename = "${path.root}/kubeconfig"
   file_permission = "0644"
   directory_permission = "0755"
 }

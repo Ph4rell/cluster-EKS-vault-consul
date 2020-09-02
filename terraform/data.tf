bdfec150511747e1aac7474e9455fc4dae6e2e03
Pierre POREE <pierre.poree@revolve.team>
2020-09-03 00:32:40 +0200

    tf remove template file

--- terraform/data.tf
+++ terraform/data.tf
@@ -1,14 +1,3 @@
 data "aws_availability_zones" "zone" {
   state = "available"
 }
-
-data "template_file" "kubeconfig" {
-  template = file("${path.module}/template/kubeconfig.tpl")
-
-  vars = {
-    endpoint                  = aws_eks_cluster.eks_cluster.endpoint
-    cluster_auth_base64       = aws_eks_cluster.eks_cluster.certificate_authority.0.data
-    cluster_name              = aws_eks_cluster.eks_cluster.name
-    aws_profile               = var.profile
-  }
-}
--- terraform/kubectl.tf
+++
@@ -1,6 +0,0 @@
-resource "local_file" "kubeconfig" {
-  content = data.template_file.kubeconfig.rendered
-  filename = "${path.root}/kubeconfig"
-  file_permission = "0644"
-  directory_permission = "0755"
-}
